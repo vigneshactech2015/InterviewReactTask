64543c57306f529d4157b9be2842898afe074ca0
vigneshactech2015 <85957001+vigneshactech2015@users.noreply.github.com>
2023-02-06 10:03:19 +0530

    Create Todo.js

---
+++ Todo.js
@@ -0,0 +1,43 @@
+import React,{useState} from 'react'
+
+function Todo() {
+    const[tasks,setTasks] = useState('')
+    const [totalTasks,setTotalTasks] = useState([])
+
+
+    const handleSubmit = (e) =>{
+        if(!tasks){
+            alert("Enter an item")
+            return
+        }
+        const item={
+            id:Math.floor(Math.random()*1000),
+            value:tasks
+        }
+        setTotalTasks(oldList=>[...oldList,item])
+        setTasks("")
+    }
+
+    function deleteItem (id){
+        const newArray = totalTasks.filter((item)=>item.id!==id)
+        setTotalTasks(newArray)
+    }
+
+  return (
+    <div>
+    <input type="text" onChange={(e)=>setTasks(e.target.value)} value={tasks}/>
+    <button type="submit" onClick={()=>handleSubmit()}>Add</button>
+   
+    <ul>
+    {totalTasks.map((item)=>{
+        return(
+            <li key={item.id}>{item.value} <button onClick={()=>deleteItem(item.id)}>D</button></li>
+        )
+    })}
+    </ul>
+
+    </div>
+  )
+}
+
+export default Todo
