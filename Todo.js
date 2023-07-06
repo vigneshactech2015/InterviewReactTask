e5f9c989108483e7263d9d1b9c96e5d443f83d1e
Vignesh <85957001+vigneshactech2015@users.noreply.github.com>
2023-07-06 22:04:12 +0530

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -1,43 +1,64 @@
-import React,{useState} from 'react'
+import { useState } from "react";
 
-function Todo() {
-    const [tasks,setTasks] = useState('')
-    const [totalTasks,setTotalTasks] = useState([])
+export default function App() {
+  const [search, setSearch] = useState("");
+  const [items, setItems] = useState([{ name: "" }]);
+  const [idx, setIdx] = useState(null);
 
+  function searchHandler(e) {
+    setSearch(e.target.value);
+  }
 
-    const handleSubmit = (e) =>{
-        if(!tasks){
-            alert("Enter an item")
-            return
-        }
-        const item={
-            id:Math.floor(Math.random()*1000),
-            value:tasks
-        }
-        setTotalTasks(oldList=>[...oldList,item])
-        setTasks("")
-    }
+  function submitHandler(e) {
+    e.preventDefault();
+    const item = {
+      value: search,
+      id: Math.floor(Math.random() * 1000)
+    };
+    setItems((prev) => [...prev, item]);
+    setSearch("");
+  }
 
-    function deleteItem (id){
-        const newArray = totalTasks.filter((item)=>item.id!==id)
-        setTotalTasks(newArray)
-    }
+  function deleteHandler(id) {
+    let filteredItem = items.filter((item) => item.id !== id);
+    setItems(filteredItem);
+  }
+
+  function editHandler(ite, i) {
+    setSearch(ite.value);
+    setIdx(i);
+  }
+  function editSubmit(e) {
+    e.preventDefault();
+    const copy = [...items];
+    copy[idx]["value"] = search;
+    setItems(copy);
+    setSearch("");
+    setIdx(null);
+  }
 
   return (
-    <div>
-    <input type="text" onChange={(e)=>setTasks(e.target.value)} value={tasks}/>
-    <button type="submit" onClick={()=>handleSubmit()}>Add</button>
-   
-    <ul>
-    {totalTasks.map((item)=>{
-        return(
-            <li key={item.id}>{item.value} <button onClick={()=>deleteItem(item.id)}>D</button></li>
-        )
-    })}
-    </ul>
-
+    <div className="App">
+      <input type="text" value={search} onChange={searchHandler} />
+      {idx === null ? (
+        <button type="submit" onClick={(e) => submitHandler(e)}>
+          Add
+        </button>
+      ) : (
+        <button type="submit" onClick={(e) => editSubmit(e)}>
+          Edit
+        </button>
+      )}
+      {items.map((item, index) => {
+        return (
+          <li key={item.id}>
+            {item.value} <span onClick={() => deleteHandler(item.id)}>X</span>
+            &nbsp;&nbsp;{" "}
+            <span onClick={() => editHandler(item, index)}>Edit</span>
+          </li>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default Todo
